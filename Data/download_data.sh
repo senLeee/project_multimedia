d8c35ccbe859e2fcc7404399957282738ac4375a
senLeee <289822875@qq.com>
2018-02-27 10:26:57 +0800

    Update download_data.sh

--- Data/download_data.sh
+++ Data/download_data.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Train_mscoco.zip
+wget -c http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip
+wget -c http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+
 wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Val_mscoco.zip
 wget -c http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip
-
-cd ..
+wget -c http://msvocds.blob.core.windows.net/coco2014/val2014.zip
